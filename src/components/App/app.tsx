a01964cac60df385a62f6244167aa39eaea012ad
Zaphaan <jerome.przybylski@gmail.com>
2021-01-10 22:45:15 +0100

    Mise en place de la structure de base

---
+++ src/components/App/app.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './app.module.css';
+
+const App = () => {
+  return (
+    <div className={styles.app}>
+      TODO:APP
+    </div>
+  );
+}
+
+export default App;
